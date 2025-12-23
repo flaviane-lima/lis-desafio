af3a3b5a4076b5ef1711300b5291008667fe5486
flaviane-lima <rflaviane41@gmail.com>
2025-12-23 11:27:29 -0300

    adicionar validação de perfil e salvar usuário com relação ao UserProfile

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,18 +1,35 @@
 import { Router } from "express";
 import { AppDataSource } from "../index";
 import { User } from "../entities/User";
+import { UserProfile } from "../entities/UserProfile";
 
 const router = Router();
 
 router.get("/", async (_, res) => {
-  const users = await AppDataSource.getRepository(User).find();
+  const users = await AppDataSource.getRepository(User).find({
+    relations: ["userProfile"],
+  });
   res.json(users);
 });
 
+//cria um usuário vinculado a um perfil
 router.post("/", async (req, res) => {
-  const user = AppDataSource.getRepository(User).create(req.body);
+ const { name, email, pass, profileId } = req.body
+  
+  const profile = await AppDataSource.getRepository(UserProfile).findOneBy({ id_userprofile: profileId });
+  if (!profile) {
+    return res.status(400).json({ message: "Perfil inválido "});
+  }
+
+  const user = AppDataSource.getRepository(User).create({
+    name,
+    email,
+    pass,
+    userProfile: profile,
+  });
+
   const result = await AppDataSource.getRepository(User).save(user);
-  res.json(result);
+  res.status(201).json(result);
 });
 
 export default router;
