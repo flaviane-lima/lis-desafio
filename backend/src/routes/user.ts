0079859f214cddde660f0436521f65333117270f
flaviane-lima <rflaviane41@gmail.com>
2025-12-23 14:32:10 -0300

    adicionar hash de senha e proteção de rotas

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { AppDataSource } from "../index";
 import { User } from "../entities/User";
 import { UserProfile } from "../entities/UserProfile";
+import bcrypt from "bcrypt"
+import { requireAuth, requireAdmin } from "../middleware/auth";
 
 const router = Router();
 
-router.get("/", async (_, res) => {
+router.get("/", requireAuth, async (_, res) => {
   const users = await AppDataSource.getRepository(User).find({
     relations: ["userProfile"],
   });
@@ -13,13 +15,15 @@ router.get("/", async (_, res) => {
 });
 
 //cria um usuário vinculado a um perfil
-router.post("/", async (req, res) => {
+router.post("/",requireAdmin, async (req, res) => {
  const { name, email, pass, profileId } = req.body
   
   const profile = await AppDataSource.getRepository(UserProfile).findOneBy({ id_userprofile: profileId });
   if (!profile) {
     return res.status(400).json({ message: "Perfil inválido "});
   }
+
+  const hashedPass = await bcrypt.hash(pass, 10);
 
   const user = AppDataSource.getRepository(User).create({
     name,
@@ -29,7 +33,10 @@ router.post("/", async (req, res) => {
   });
 
   const result = await AppDataSource.getRepository(User).save(user);
-  res.status(201).json(result);
+
+  //remove senha do retorno
+  const { pass: _, ... safeUser } = result
+  res.status(201).json(safeUser);
 });
 
 export default router;
